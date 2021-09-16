3e8ba19a0747dd82cf6eb0358f313df7409386e3
IPlayZed <pauljouser@gmail.com>
2021-09-16 12:21:29 +0200

    Generated header component and added it to app page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-showcase';
+  title: string = 'angular-showcase';
 }
